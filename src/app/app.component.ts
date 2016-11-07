1e5043dd457636db2e35e0cddfc2278f5dcfdc72
Dean <Dean>
2016-11-07 09:54:02 +0530

    add files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import {ChatService} from './components/chat/chat.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ChatService]
 })
 export class AppComponent {
-  title = 'app works!';
+  
 }
